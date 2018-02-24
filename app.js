157c45e57c303263c55ce0da7895a268fdbd8b74
Jedwardnyc <jacobedwardrico@gmail.com>
2018-02-24 17:02:53 -0500

    cleaned up functions and added readme file

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+const custList = document.getElementById('customerList');
+const email = document.getElementById('email')
+
 fetch('/api/customers')
   .then(result=> result.json())
   .then((data)=>{
-    const custList = document.getElementById('customerList');
     data.forEach((customer)=>{
-      const li = document.createElement('li');
-      const form = document.createElement('form');
-      custList.append(form)
-      li.append(customer.email);
-      form.setAttribute('method', 'POST')
-      form.setAttribute('action', `/api/customers/${customer.id}?_method=DELETE`)
-      li.addEventListener('click', function(e){
-        console.log(customer)
-        form.submit()
-        li.remove();
-      })
-      form.append(li);
+      createCustomer(customer)
     });
   });
 
+createCustomer = (customer)=>{
+  const li = document.createElement('li');
+  const form = document.createElement('form');
+  custList.append(form)
+  li.append(customer.email);
+  form.setAttribute('method', 'POST')
+  form.setAttribute('action', `/api/customers/${customer.id}?_method=DELETE`)
+  li.addEventListener('click', function(e){
+    form.submit()
+    li.remove();
+  })
+  form.append(li);
+}
--- server.js
+++ server.js
@@ -9,26 +9,28 @@ app.use(require('body-parser').json());
 app.use(require('body-parser').urlencoded({ extended:false }));
 app.use(require('method-override')('_method'))
 
-
 app.get('/', (req,res,next)=>{
   res.sendFile('index.html');
 });
 
 app.get('/api/customers', (req,res,next)=>{
   Customer.findAll()
-    .then((customers)=> res.json(customers));
+    .then((customers)=> res.send(customers))
+    .catch(next)
 });
 
 app.post('/api/customers', (req,res,next)=>{
   Customer.create(req.body)
     .then((customer)=> res.json(customer))
-  res.redirect('/')
+    res.redirect('/')
+    .catch(next)
 })
 
 app.delete('/api/customers/:id', (req,res,next)=>{
   Customer.findById(req.params.id)
     .then((customer)=> customer.destroy())
-  res.redirect('/');
+    res.redirect('/')
+    .catch(next)
 });
 
 const port = process.env.PORT || 3000 
